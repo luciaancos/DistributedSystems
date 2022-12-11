9a3412569a6d6c9f2714e71afc34e95346f1fce7
luciaancos <92110037+luciaancos@users.noreply.github.com>
2022-12-11 21:26:01 +0100

    ordinaria-p1

--- iceflix/cli.py
+++ iceflix/cli.py
@@ -4,6 +4,7 @@ import logging
 import sys
 
 from iceflix.main import MainApp
+from iceflix.client import Client
 
 
 LOG_FORMAT = '%(asctime)s - %(levelname)-7s - %(module)s:%(funcName)s:%(lineno)d - %(message)s'
@@ -17,36 +18,9 @@ def setup_logging():
     )
 
 
-def main_service():
-    """Handles the `mainservice` CLI command."""
-    setup_logging()
-    logging.info("Main service starting...")
-    sys.exit(MainApp().main(sys.argv))
-
-
-def catalog_service():
-    """Handles the `catalogservice` CLI command."""
-    setup_logging()
-    logging.info("Catalog service")
-    return 0
-
-
-def file_service():
-    """Handles the `streamingservice` CLI command."""
-    setup_logging()
-    logging.info("File service")
-    return 0
-
-
-def authentication_service():
-    """Handles the `authenticationservice` CLI command."""
-    setup_logging()
-    logging.info("Authentication service")
-    return 0
-
-
 def client():
     """Handles the IceFlix client CLI command."""
     setup_logging()
     logging.info("Starting IceFlix client...")
-    return 0
+    sys.exit(Client().main(sys.argv))
+    
